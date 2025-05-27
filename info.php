64cb9cafba83ce2ada53cc834d8ffa87f675f18f
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2025-05-27 03:02:34 +0100

    Update info.php

--- info.php
+++ info.php
@@ -3,11 +3,9 @@ defined('BASEPATH') OR die;
 
 return array(
 	'name'         => 'Default',
-	'theme_uri'    => 'https://github.com/bkader/skeleton/tree/develop/public/content/themes',
+	'theme_uri'    => 'https://github.com/ianhubnet/csk-theme-default/',
 	'description'  => 'Default theme that comes with the CodeIgniter Skeleton.',
 	'version'      => '1.0',
-	'license'      => 'MIT',
-	'license_uri'  => 'https://opensource.org/licenses/MIT',
 	'author'       => 'Kader Bouyakoub',
 	'author_uri'   => 'http://bit.ly/KaderGhb/',
 	'author_email' => 'bkader@mail.com',
