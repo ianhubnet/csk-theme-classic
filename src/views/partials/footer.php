0d9e881359a15dd12c570757db8d1a2915f5e307
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2025-10-15 17:34:44 +0100

    Update footer.php

--- src/views/partials/footer.php
+++ src/views/partials/footer.php
@@ -1,11 +1,12 @@
 <footer class="navbar fixed-bottom bg-body-secondary" role="contentinfo">
-	<div class="container justify-content-center">
-		<p class="navbar-text p-0 m-1"><?= anchor('', $site_name) ?>. &copy; <?= line('all_rights_reserved') ?> <?= date('Y') ?>.<?php if (!CI_LIVE && $this->hub->auth->is_admin()): ?> <abbr title="Render Time">RT</abbr>: <strong>{elapsed_time}</strong>. <abbr title="Theme Render Time">TT</abbr>: <strong>{theme_time}</strong>.<?php endif; ?> <?= sline('powered_by_url', Platform::LABEL, Platform::SITE_URL) ?></p>
-
-<?= $this->hub->menus->render('footer', [
-	'menu_attrs' => ['class' => 'nav nav-pills nav-underline ms-2'],
-	'item_attr' => ['class' => 'nav-item'],
-	'link_attr' => ['class' => 'nav-link'],
-]) ?>
-	</div>
+	<div class="container d-flex flex-column flex-md-row justify-content-center gap-1 gap-md-3">
+		<p class="navbar-text text-center text-md-start m-0 p-0">
+			<?= anchor('', $site_name) ?>. &copy; <?= line('all_rights_reserved') ?> <?= date('Y') ?>.<?php if (!CI_LIVE && $this->hub->auth->is_admin()): ?> <abbr title="Render Time">RT</abbr>: <strong>{elapsed_time}</strong>. <abbr title="Theme Render Time">TT</abbr>: <strong>{theme_time}</strong>.<?php endif; ?> <?= sline('powered_by_url', Platform::LABEL, Platform::SITE_URL) ?>
+		</p>
+		<?= $this->hub->menus->render('footer', [
+			'menu_attrs' => ['class' => 'nav flex-row gap-2'],
+			'item_attr' => ['class' => 'nav-item'],
+			'link_attr' => ['class' => 'nav-link'],
+		]) ?>
+	</div><!--/.container-->
 </footer>
