435c10ba1c3fd3d7529c770d3388613bc93dcd1a
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2025-12-08 03:46:59 +0100

    chore(lang): Added AI generated Croatian and Slovak

---
+++ src/language/croatian.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['menu_loc_main'] = 'Glavni izbornik';
+$lang['menu_loc_footer'] = 'Izbornik podnožja';
+$lang['menu_loc_sidebar'] = 'Bočni izbornik';
+
+$lang['theme_sidebar_heading'] = 'Prevedeni naslov';
+$lang['theme_sidebar_content'] = 'Bočna traka zadane teme';
---
+++ src/language/slovak.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['menu_loc_main'] = 'Hlavné menu';
+$lang['menu_loc_footer'] = 'Menu päty';
+$lang['menu_loc_sidebar'] = 'Bočné menu';
+
+$lang['theme_sidebar_heading'] = 'Preložený nadpis';
+$lang['theme_sidebar_content'] = 'Bočný panel predvolenej témy';
