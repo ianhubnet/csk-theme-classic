741868753d050a3386332910e4c272e3a4c276af
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2025-12-19 00:34:00 +0100

    chore(lang): Added AI generated Bengali and Catalan

---
+++ src/language/bengali.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['menu_loc_main'] = 'প্রধান মেনু';
+$lang['menu_loc_footer'] = 'ফুটার মেনু';
+$lang['menu_loc_sidebar'] = 'সাইডবার মেনু';
+
+$lang['theme_sidebar_heading'] = 'অনুবাদিত শিরোনাম';
+$lang['theme_sidebar_content'] = 'ডিফল্ট থিম সাইডবার';
---
+++ src/language/catalan.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['menu_loc_main'] = 'menú principal';
+$lang['menu_loc_footer'] = 'menú de peu de pàgina';
+$lang['menu_loc_sidebar'] = 'menú de la barra lateral';
+
+$lang['theme_sidebar_heading'] = 'Encapçalament traduït';
+$lang['theme_sidebar_content'] = 'Barra lateral del tema clàssic';
