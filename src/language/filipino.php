12f2a893f2bf87870217f7a4b5ffe61ed44ebb89
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-01-04 04:00:01 +0100

    chore(lang): Added AI generated Filipino

---
+++ src/language/filipino.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['menu_loc_main'] = 'Pangunahing Menu';
+$lang['menu_loc_footer'] = 'Menu ng Paa';
+$lang['menu_loc_sidebar'] = 'Menu ng Sidebar';
+
+$lang['theme_sidebar_heading'] = 'Isinalin na Pamagat';
+$lang['theme_sidebar_content'] = 'Default na Sidebar ng Tema';
