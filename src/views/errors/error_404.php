5cd31c360863613fd71eae063c55f16efe3ee07d
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-04-16 03:18:18 +0100

    fix(404): fix error 404 view

--- src/views/errors/error_404.php
+++ src/views/errors/error_404.php
@@ -1,20 +1,18 @@
-<div class="error-outer">
-	<div class="error-middle">
-		<div class="error-inner">
-			<div class="error-circle">
-				<i class="fa fa-fw fa-home"></i>
-				<span>404</span>
-			</div>
-			<span class="error-status"><?= $heading ?></span>
-			<span class="error-detail"><?= $message ?></span>
+<div class="row">
+	<div class="col">
+		<div class="px-4 py-3">
+			<h1 class="h2 fw-bold text-body-emphasis text-center"><?= $heading ?></h1>
+			<p class="lead text-center mt-2 mb-0"><?= $message ?></p>
 			<div class="d-grid d-md-flex gap-3 justify-content-center mt-3">
 				<?php
+				// Back to homepage.
 				echo anchor('', line('return_home'), 'class="btn btn-primary"');
+
 				if ($this->module->enabled('contact')) {
 					echo anchor('contact', line('contact_us'), 'class="btn btn-light"');
 				}
 				?>
 			</div>
 		</div>
-	</div>
-</div>
+	</div><!--/.col-->
+</div><!--/.row-->
