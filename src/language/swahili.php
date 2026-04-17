6fe6b8ebb15ed820b58cfd4707e3966cc578b489
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-04-18 00:52:57 +0100

    feat(lang): add 'swahili' language file

---
+++ src/language/swahili.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['menu_loc_main'] = 'Menyu Kuu';
+$lang['menu_loc_footer'] = 'Menyu ya Chini';
+$lang['menu_loc_sidebar'] = 'Menyu ya Upande';
+
+$lang['theme_sidebar_heading'] = 'Kichwa Kilichotafsiriwa';
+$lang['theme_sidebar_content'] = 'Menyu ya Upande ya Mandhari ya Kawaida';
