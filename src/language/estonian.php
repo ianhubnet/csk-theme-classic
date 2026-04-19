38e493e57b19628c481e5769726becb1d3febd30
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-04-19 23:03:36 +0100

    feat(lang): add AI generate Estonian translation

---
+++ src/language/estonian.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['menu_loc_main'] = 'Peamenüü';
+$lang['menu_loc_footer'] = 'Jalamenüü';
+$lang['menu_loc_sidebar'] = 'Külgmenüü';
+
+$lang['theme_sidebar_heading'] = 'Tõlgitud pealkiri';
+$lang['theme_sidebar_content'] = 'Vaikimisi teema külgmenüü';
