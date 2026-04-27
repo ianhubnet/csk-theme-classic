42f16d7bd65142211fca908aed75130e8dc63c91
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-04-27 03:17:56 +0100

    feat(lang): add Google-Translated "georgian"

---
+++ src/language/georgian.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['menu_loc_main'] = 'მთავარი მენიუ';
+$lang['menu_loc_footer'] = 'ფეხის მენიუ';
+$lang['menu_loc_sidebar'] = 'მხრის მენიუ';
+
+$lang['theme_sidebar_heading'] = 'გადათარგმნილი სათაური';
+$lang['theme_sidebar_content'] = 'ნაგულისხმევი თემის მხრის მენიუ';
