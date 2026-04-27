7f6cd45404dcb8f7f96886a93223a119993886a4
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-04-27 03:43:48 +0100

    feat(lang): add Google-Translated "armenian"

---
+++ src/language/armenian.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['menu_loc_main'] = 'Գլխավոր մենյու';
+$lang['menu_loc_footer'] = 'Վերջնամասի մենյու';
+$lang['menu_loc_sidebar'] = 'Կողային մենյու';
+
+$lang['theme_sidebar_heading'] = 'Թարգմանված վերնագիր';
+$lang['theme_sidebar_content'] = 'Ստանդարտ թեմայի կողային մենյու';
